c3ff117bf43733f74467da72c51db67d55c00ded
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-11-01 17:24:45 -0400

    all tests are working properly

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,13 +2,28 @@ import React from 'react';
 import { render } from "@testing-library/react";
 import App from './App';
 import DisplayData from './Components/DisplayData';
+import NavBar from './Components/NavBar';
+import useDarkMode from './hooks/useDarkMode';
+import useLocalStorage from "./hooks/useLocalStorage";
 
 test("DisplayData is working correctly", () => {
   const data = [
     { name: "Alex Morgan", country: "United States", searches: 100 }
   ];
   render(<DisplayData data={data}/>)
-}) 
+});
+
+test("NavBar is working correctly", () => {
+  render(<NavBar/>);
+});
+
+test("useDarkMode is working correctly", () => {
+  render(<useDarkMode/>);
+});
+
+test("useLocalStorage is working correctly", () => {
+  render(<useLocalStorage/>);
+});
 
 test('renders without crashing', () => {
   render(<App />);
