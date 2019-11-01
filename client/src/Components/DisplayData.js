5c55bd123eb9e9998e93f07fb4f082549104655d
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-11-01 16:15:18 -0400

    styling

--- client/src/Components/DisplayData.js
+++ client/src/Components/DisplayData.js
@@ -10,7 +10,7 @@ function DisplayData(props) {
     <div className="card-container">
       {props.data.map(data => (
         <div key={data.id} className="person-card">
-          <h1>Name: {data.name}</h1>
+          <h2>Name: {data.name}</h2>
           <p>Country: {data.country}</p>
           <p>Searches: {data.searches}</p>
         </div>
